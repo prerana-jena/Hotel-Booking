164ddd1f82a7e16325ca15f093e47117038d053c
prerana-jena <prerana.li011@gmail.com>
2026-07-08 00:46:08 +0530

    fix

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const userSchema=new mongoose.Schema({
     _id: {type: String, required: true},
-    usename: {type: String, required: true},
+    username: {type: String, required: true},
     email: {type: String, required: true},
     image: {type: String, required: true},
     role: {type: String, enum: ["user","hotel owner"],default: "user"},
